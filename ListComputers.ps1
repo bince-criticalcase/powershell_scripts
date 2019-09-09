edde65937cc0050562c048c524f75c2cf7cd5eed
cimitrasoftware <55113746+cimitrasoftware@users.noreply.github.com>
2019-09-09 15:14:15 -0700

    Add files via upload

---
+++ ListComputers.ps1
@@ -0,0 +1,12 @@
+﻿Write-Output ""
+Write-Output "Following is a list of all of the computers, newest to oldest."
+Write-Output "------------------------------------------------------"
+function reverse
+{ 
+ $arr = @(Get-ADComputer -Filter * -SearchBase "OU=COMPUTERS,OU=DEMO,OU=CIMITRA,DC=cimitrademo,DC=com" | Select Name) 
+ [array]::reverse($arr)
+ $arr
+}
+reverse
+Write-Output ""
+Write-Output "------------------------------------------------------"
