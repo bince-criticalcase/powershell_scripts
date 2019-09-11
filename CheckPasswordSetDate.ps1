d9869b8446c24966f5b123aedbeb724954a5c4a6
cimitrasoftware <55113746+cimitrasoftware@users.noreply.github.com>
2019-09-11 10:29:28 -0600

    Add files via upload

---
+++ CheckPasswordSetDate.ps1
@@ -0,0 +1,9 @@
+﻿$firstNameIn=$args[0]
+$lastNameIn=$args[1]
+
+ $theResult=Get-ADUser -properties PasswordLastSet  -Identity "CN=${firstNameIn} ${lastNameIn},OU=USERS,OU=DEMO,OU=CIMITRA,DC=cimitrademo,DC=com" | Select-Object PasswordLastSet -ExpandProperty PasswordLastSet
+ Write-Output "------------------------------------------------------------------------------"
+ Write-Output ""
+ Write-Output "Last Password Reset for User: ${firstNameIn} ${LastNameIn} was on ${theResult}"
+ Write-Output ""
+ Write-Output "------------------------------------------------------------------------------"
