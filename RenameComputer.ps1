0c7b47ce0f1f58446333f02a5c5c65163204720a
cimitrasoftware <55113746+cimitrasoftware@users.noreply.github.com>
2019-10-04 09:57:48 -0600

    Add files via upload

---
+++ RenameComputer.ps1
@@ -0,0 +1,21 @@
+﻿# Change the context variable to match your system
+# -------------------------------------------------
+
+$context = "OU=COMPUTERS,OU=DEMO,OU=CIMITRA,DC=cimitrademo,DC=com" 
+
+# -------------------------------------------------
+$theComputerOldNameIn = $args[0]
+$theComputerNewNameIn = $args[1]
+
+Rename-ADObject -Identity "CN=$theComputerOldNameIn,$context" -NewName "$theComputerNewNameIn"  
+$theResult = $?
+
+if ($theResult)
+{
+Write-Output "--------------------------------------------------------"
+Write-Output ""
+Write-Output "The Computer: $theComputerOldNameIn was renamed to $theComputerNewNameIn"
+Write-Output ""
+Write-Output "--------------------------------------------------------"
+
+}
