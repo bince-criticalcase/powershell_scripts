ecad6d993cd496fdc50fd68f68d8e2c629112f07
cimitrasoftware <55113746+cimitrasoftware@users.noreply.github.com>
2019-10-07 03:01:01 -0600

    Add files via upload

---
+++ SetUserPassword.ps1
@@ -0,0 +1,19 @@
+﻿# Change the context variable to match your system
+# -------------------------------------------------
+
+$context = "OU=USERS,OU=DEMO,OU=CIMITRA,DC=cimitrademo,DC=com" 
+
+# -------------------------------------------------
+$firstNameIn=$args[0]
+$lastNameIn=$args[1]
+$newPasswordIn=$args[2]
+
+Set-ADAccountPassword -Identity "CN=${firstNameIn} ${lastNameIn},$context" -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "$newPasswordIn" -Force)
+
+$theResult=Get-ADUser -properties PasswordLastSet  -Identity "CN=${firstNameIn} ${lastNameIn},$context" | Select-Object PasswordLastSet -ExpandProperty PasswordLastSet
+ 
+ Write-Output "------------------------------------------------------------------------------"
+ Write-Output ""
+ Write-Output "Password Reset for User: ${firstNameIn} ${lastNameIn} on ${theResult}"
+ Write-Output ""
+ Write-Output "------------------------------------------------------------------------------"
